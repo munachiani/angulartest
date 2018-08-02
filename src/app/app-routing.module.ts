c78e8091cca02a5d6c2132cea364e003b3e7e095
munachiani <mailstomuna@gmail.com>
2018-08-02 03:34:15 -0700

    chapter03 initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import { BeersComponent} from './beers/beers.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'beers', pathMatch: 'full' },
+  { path: 'beers', component: BeersComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
